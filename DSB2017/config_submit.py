bc3f24ba81b4b36ef33febbd7250874305af9009
Andor Vári-Kakas <varikakasandor@gmail.com>
2021-08-09 10:46:15 +0000

    Config file changed

--- DSB2017/config_submit.py
+++ DSB2017/config_submit.py
@@ -1,14 +1,14 @@
 config = {
     'datapath':'/mnt/idms/PROJECTS/Lung/LIDC-IDRI',
-    'preprocess_result_path':'/mnt/idms/home/andor/DSB2017/prep_result/',
-    'outputfile':'/mnt/idms/home/andor/DSB2017/prediction.csv',
+    'preprocess_result_path':'/mnt/idms/PROJECTS/Lung/LungCT/DSB2017/prep_result/',
+    'outputfile':'/mnt/idms/PROJECTS/Lung/LungCT/DSB2017/prediction.csv',
     'detector_model':'net_detector',
-    'detector_param':'/mnt/idms/home/andor/DSB2017/model/detector.ckpt',
+    'detector_param':'/mnt/idms/PROJECTS/Lung/LungCT/DSB2017/model/detector.ckpt',
     'classifier_model':'net_classifier',
-    'classifier_param':'/mnt/idms/home/andor/DSB2017/model/classifier.ckpt',
-    'n_gpu':10,
-    'n_worker_preprocessing':1,
+    'classifier_param':'/mnt/idms/PROJECTS/Lung/LungCT/DSB2017/model/classifier.ckpt',
+    'n_gpu':10, #Too big would run out of CUDA memory
+    'n_worker_preprocessing':60,
     'use_exsiting_preprocessing':True,
-    'skip_preprocessing': True, #False,
+    'skip_preprocessing': True,
     'skip_detect': True
 }
